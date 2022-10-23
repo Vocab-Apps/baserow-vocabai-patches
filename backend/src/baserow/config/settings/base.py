7f79a5eea9bcc9d522b4785e0c161be9cd8c5dcc
luc wastiaux <luc.wastiaux@airpost.net>
2022-10-23 09:59:21 +0800

    print details about transactions

--- backend/src/baserow/config/settings/base.py
+++ backend/src/baserow/config/settings/base.py
@@ -8,6 +8,7 @@ from decimal import Decimal
 from ipaddress import ip_network
 from pathlib import Path
 from urllib.parse import urljoin, urlparse
+import pprint
 
 from django.core.exceptions import ImproperlyConfigured
 
@@ -53,7 +54,8 @@ from sentry_sdk.integrations.celery import CeleryIntegration
 
 def traces_sampler(sampling_context):
     if sampling_context.get('asgi_scope', {}).get('path', None) == '/_health/':
-        return 0.0
+        return 0.001
+    pprint.pprint(sampling_context)
     return 1.0
 
 sentry_sdk.init(
