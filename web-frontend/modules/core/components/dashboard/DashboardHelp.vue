a988638189a277f389c8f95f98b24c186dab70ac
Luc W <lucw@airpost.net>
2023-05-07 14:11:00 +0800

    link to baserow.io

--- web-frontend/modules/core/components/dashboard/DashboardHelp.vue
+++ web-frontend/modules/core/components/dashboard/DashboardHelp.vue
@@ -1,78 +1,13 @@
 <template>
-  <Alert
-    v-if="displayAlert"
-    type="blank"
-    close-button
-    position="bottom"
-    :width="396"
-    @close="handleAlertClose"
-  >
-    <template #image
-      ><img
-        src="@baserow/modules/core/assets/images/dashboard_alert_image.png"
-        srcset="
-          @baserow/modules/core/assets/images/dashboard_alert_image@2x.png 2x
-        "
-      />
-    </template>
-    <template #title
-      ><h4>{{ $t('dashboard.alertTitle') }}</h4></template
-    >
-    <p>{{ $t('dashboard.alertText') }}</p>
-    <template #actions>
-      <Button
-        tag="a"
-        href="https://gitlab.com/baserow/baserow"
-        target="_blank"
-        rel="noopener noreferrer"
-        type="secondary"
-        icon="baserow-icon-gitlab"
-      >
-        {{ $t('dashboard.starOnGitlab') }}</Button
-      >
-      <ButtonIcon
-        v-tooltip="$t('dashboard.shareOnTwitter')"
-        tag="a"
-        tooltip-position="top"
-        :href="`https://twitter.com/intent/tweet?url=https://baserow.io&hashtags=opensource,nocode,database,baserow&text=${encodeURI(
-          $t('dashboard.tweetContent')
-        )}`"
-        target="_blank"
-        rel="noopener noreferrer"
-        icon="baserow-icon-twitter"
-      ></ButtonIcon>
-      <ButtonIcon
-        v-tooltip="$t('dashboard.shareOnReddit')"
-        tag="a"
-        tooltip-position="top"
-        icon="baserow-icon-reddit"
-        :href="
-          'https://www.reddit.com/submit?url=https://baserow.io&title=' +
-          encodeURI($t('dashboard.redditTitle'))
-        "
-        target="_blank"
-        rel="noopener noreferrer"
-      ></ButtonIcon>
-      <ButtonIcon
-        v-tooltip="$t('dashboard.shareOnFacebook')"
-        tag="a"
-        tooltip-position="top"
-        icon="baserow-icon-facebook"
-        href="https://www.facebook.com/sharer/sharer.php?u=https://baserow.io"
-        target="_blank"
-        rel="noopener noreferrer"
-      ></ButtonIcon>
-      <ButtonIcon
-        v-tooltip="$t('dashboard.shareOnLinkedIn')"
-        tag="a"
-        tooltip-position="top"
-        icon="baserow-icon-linkedin"
-        href="https://www.linkedin.com/sharing/share-offsite/?url=https://baserow.io"
-        target="_blank"
-        rel="noopener noreferrer"
-      ></ButtonIcon>
-    </template>
-  </Alert>
+  <div class="dashboard__help">
+    <img src="@baserow/modules/core/static/img/vocabai_banner.png" alt="" style="height: 80px;"/>
+    <p class="dashboard__help-description">
+      Introducing <b>Vocab.Ai</b> - The revolutionary language learning spreadsheet designed to supercharge your vocabulary and sentence mastery!
+    </p>
+    <p> class="dashboard__help-description">
+      <a href="https://lucw.medium.com/unlock-the-power-of-pinyin-vocab-ais-edc5db83a1a9" target="_blank">Chinese and Pinyin tutorial</a>
+    </p>
+    <i>note: Vocab.Ai is based on open-source database <a href="https://baserow.io" target="_blank">Baserow</a></i>
 </template>
 
 <script>
