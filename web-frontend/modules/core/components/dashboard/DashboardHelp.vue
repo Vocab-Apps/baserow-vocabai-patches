d984975289d4f72f50ed402099de4e6165dc41e8
Luc W <lucw@airpost.net>
2023-05-07 14:04:22 +0800

    reformat, change banner height

--- web-frontend/modules/core/components/dashboard/DashboardHelp.vue
+++ web-frontend/modules/core/components/dashboard/DashboardHelp.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="dashboard__help">
-    <img src="@baserow/modules/core/static/img/vocabai_banner.png" alt="" />
+    <img src="@baserow/modules/core/static/img/vocabai_banner.png" alt="" style="height: 80px;"/>
     <p class="dashboard__help-description">
-      Vocab.Ai allows you to manage your language learning vocabulary and sentence lists.
-      | <a href="https://lucw.medium.com/unlock-the-power-of-pinyin-vocab-ais-edc5db83a1a9"></a>Chinese and Pinyin tutorial</a>
+      Introducing <b>Vocab.Ai</b> - The revolutionary language learning spreadsheet designed to supercharge your vocabulary and sentence mastery!
     </p>
-  </div>
+    <p> class="dashboard__help-description">
+      <a href="https://lucw.medium.com/unlock-the-power-of-pinyin-vocab-ais-edc5db83a1a9" target="_blank">Chinese and Pinyin tutorial</a>
+    </p>
 </template>
 
 <script>
