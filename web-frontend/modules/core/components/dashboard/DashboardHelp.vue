756e8b804ecbb110d0a0190a0fc32085d1ddcf4c
Luc W <lucw@airpost.net>
2023-05-07 16:02:01 +0800

    reformat dashboard

--- web-frontend/modules/core/components/dashboard/DashboardHelp.vue
+++ web-frontend/modules/core/components/dashboard/DashboardHelp.vue
@@ -3,11 +3,11 @@
     <img src="@baserow/modules/core/static/img/vocabai_banner.png" alt="" style="height: 80px;"/>
     <p class="dashboard__help-description">
       Introducing <b>Vocab.Ai</b> - The revolutionary language learning spreadsheet designed to supercharge your vocabulary and sentence mastery!
-    </p>
-    <p> class="dashboard__help-description">
+      <br/>
       <a href="https://lucw.medium.com/unlock-the-power-of-pinyin-vocab-ais-edc5db83a1a9" target="_blank">Chinese and Pinyin tutorial</a>
+      <br/>
+      <i>note: Vocab.Ai is based on open-source database <a href="https://baserow.io" target="_blank">Baserow</a></i>
     </p>
-    <i>note: Vocab.Ai is based on open-source database <a href="https://baserow.io" target="_blank">Baserow</a></i>
   </div>
 </template>
 
