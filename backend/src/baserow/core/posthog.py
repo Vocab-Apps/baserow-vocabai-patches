131867dad29dd42afdeb744ef49c4a475f53a21b
Luc W <lucw@airpost.net>
2024-12-30 15:53:58 +0800

    user email is distinct_id

--- backend/src/baserow/core/posthog.py
+++ backend/src/baserow/core/posthog.py
@@ -77,7 +77,7 @@ def capture_user_event(
     if workspace is not None:
         properties["workspace_id"] = workspace.id
 
-    capture_event(user_id, event, properties)
+    capture_event(user.email, event, properties)
 
 
 @receiver(action_done)
